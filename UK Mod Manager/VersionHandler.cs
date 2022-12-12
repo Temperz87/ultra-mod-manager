aee9fe1a24ecf22eba8a170193bbf49a43edfd85
Temperz87 <58375877+Temperz87@users.noreply.github.com>
2022-12-12 15:24:37 -0500

    bump version
    Version is now 0.5.0

--- UK Mod Manager/VersionHandler.cs
+++ UK Mod Manager/VersionHandler.cs
@@ -7,7 +7,7 @@ namespace UMM.Loader
 {
     public static class VersionHandler
     {
-        public const string VERSION = "0.4.4"; // Should this be hardcoded? No it should not be
+        public const string VERSION = "0.5.0"; // Should this be hardcoded? No it should not be
         public static  IEnumerator CheckVersion()
         {
             Plugin.logger.LogInfo("Trying to get verison.");
